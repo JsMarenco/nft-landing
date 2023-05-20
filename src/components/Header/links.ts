68b44490df61a3fcf4b5710402dfd41a4ae0905e
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-20 16:03:49 -0600

    Update: Added links to the navigation

---
+++ src/components/Header/links.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    label: "Home",
+    href: "#",
+  },
+  {
+    label: "About",
+    href: "#",
+  },
+  {
+    label: "Collection",
+    href: "#",
+  },
+  {
+    label: "Blog",
+    href: "#",
+  },
+  {
+    label: "Contact",
+    href: "#",
+  },
+]
