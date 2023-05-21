60a0ec2f399cd735fc72bdde5f2e2c9e2c1cfa97
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-20 21:12:37 -0600

    Add: Added data for sliders

---
+++ src/components/Hero/sliders.ts
@@ -0,0 +1,39 @@
+import { SliderImage } from "@/ts/interfaces"
+import img1 from "@/assets/slider/1.jpg"
+import img2 from "@/assets/slider/2.jpg"
+import img3 from "@/assets/slider/3.jpg"
+import img4 from "@/assets/slider/4.jpg"
+import img5 from "@/assets/slider/5.jpg"
+import img6 from "@/assets/slider/6.jpg"
+import img7 from "@/assets/slider/7.jpg"
+
+export const sliders: SliderImage[] = [
+  {
+    src: img1.src,
+    name: "Ronaldo Legros",
+  },
+  {
+    src: img2.src,
+    name: "Madge Turner",
+  },
+  {
+    src: img3.src,
+    name: "Louvenia Purdy",
+  },
+  {
+    src: img4.src,
+    name: "Reina Howell",
+  },
+  {
+    src: img5.src,
+    name: "Zora Rau",
+  },
+  {
+    src: img6.src,
+    name: "Myrna Emmerich",
+  },
+  {
+    src: img7.src,
+    name: "Gilberto McDermott",
+  },
+]
