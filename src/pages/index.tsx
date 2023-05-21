d529e61d4425c20dcd4d2c8819ae1c0b6cd77d8c
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-20 21:15:31 -0600

    Add: Added ScrollTop to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
+import ScrollTop from "@/components/Buttons/ScrollTop"
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+
+      <ScrollTop />
     </>
   )
 }
