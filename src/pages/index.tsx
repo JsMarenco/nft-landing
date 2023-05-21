aaad43446e36d300f7ea85cadc48c693d2348d24
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-20 22:09:40 -0600

    Add: Added Footer section to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import ScrollTop from "@/components/Buttons/ScrollTop"
 import Info from "@/components/Info"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Info />
+      <Footer />
 
       <ScrollTop />
     </>
