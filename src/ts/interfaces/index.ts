05b7bd9ed62546797b580744ead7b15d7820c500
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-20 21:08:49 -0600

    Add: Added SliderControlsProps interface

--- src/ts/interfaces/index.ts
+++ src/ts/interfaces/index.ts
@@ -6,3 +6,10 @@ export interface SliderImage {
 export interface SliderProps {
   images: SliderImage[]
 }
+
+export type SliderAction = "next" | "prev"
+
+export interface SliderControlsProps {
+  // eslint-disable-next-line no-unused-vars
+  handleSlide: (action: SliderAction) => void
+}
