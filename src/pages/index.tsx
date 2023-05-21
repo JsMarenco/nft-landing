f6a7a341bc72f96d9bd437ac221164fa2ed2700c
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-20 21:50:36 -0600

    Add: Added Info section to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import ScrollTop from "@/components/Buttons/ScrollTop"
+import Info from "@/components/Info"
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Info />
 
       <ScrollTop />
     </>
